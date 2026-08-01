3ba6796517864e4259634a44c33d8fc168396dbb
Royce J. Porter <royce@cognitivesecurityinstitute.org>
2026-08-01 13:51:29 -0400

    Add Hello World Python program
    This program prints a welcome message and introduces the Security Engineering Labs.

---
+++ Python/Programming100/Exercises/01_Hello_World/main.py
@@ -0,0 +1,12 @@
+"""
+Exercise 01 - Hello World
+
+Author: Royce J. Porter
+
+Description:
+A simple Python program demonstrating the basic structure of a Python application.
+"""
+
+print("Hello, World!")
+print("Welcome to the Security Engineering Labs.")
+print("Building engineering skills one project at a time.")
