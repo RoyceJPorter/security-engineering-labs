e7e1164212d0a4207ff24991c45e31d89f78f3b7
Royce J. Porter <royce@cognitivesecurityinstitute.org>
2026-08-01 18:09:07 -0400

    Add Exercise 02 variables program

---
+++ Python/Programming100/Exercises/02_Variables/main.py
@@ -0,0 +1,22 @@
+"""
+Exercise 02 - Variables
+Programming 100
+Author: Royce J. Porter
+"""
+
+# String variable
+name = "Royce"
+
+# Integer variable
+years_of_service = 15
+
+# Boolean variable
+veteran = True
+
+# Float variable
+gpa = 4.0
+
+print("Name:", name)
+print("Years of Service:", years_of_service)
+print("Veteran:", veteran)
+print("Current GPA:", gpa)
